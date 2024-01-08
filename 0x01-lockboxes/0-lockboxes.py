109881b9f8caf34a050319154871b6629da2c393
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-08 17:41:57 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened """
+    if boxes is None:
+        return False
+    if len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
+        return True
+    return False
