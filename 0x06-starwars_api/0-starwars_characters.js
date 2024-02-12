77a1e67bd5fef02503482314dd227fc93a893403
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-02-12 13:43:46 +0100

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require("request");
+const url = "https://swapi-api.hbtn.io/api/films/";
+const pross = require("process");
+
+if (pross.argv.length > 2) {
+  request(url + pross.argv[2], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, (promiseErr, __, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        })
+    );
+
+    Promise.all(charactersName)
+      .then((names) => console.log(names.join("\n")))
+      .catch((allErr) => console.log(allErr));
+  });
+}
